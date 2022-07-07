99e4dd33e8e51e961116bd2f68ba6b1d46334553
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-07 14:51:35 +0700

    Update Grup-seting.js

--- plugins/Grup-seting.js
+++ plugins/Grup-seting.js
@@ -8,6 +8,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 *Format salah! Contoh :*
   *○ ${usedPrefix + command} close*
   *○ ${usedPrefix + command} open*
+⭐ ${usedPrefix + command} close Tidak akan work jika
+grub dalam kondisi tertutup, harus menulis format .group open
+
+⭐ ${usedPrefix + command} open Tidak akan work jika
+Grub masih terbuka, harus menulis format .group close
+
+║▌│█║▌│ █║▌│█│║▌║
 `.trim()
     await conn.groupSettingUpdate(m.chat, isClose)
 }
