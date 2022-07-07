a561bccc49beb25516e04d747dfc20b2d0da0ddb
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-07 12:28:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'Created by IrsannGanzz'
+global.wm = 'FantasyBot By Owner'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
