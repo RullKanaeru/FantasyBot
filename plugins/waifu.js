587c40c78eca1eeb4255a8ea90ef4827c5c5b2f2
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-07 07:49:30 +0700

    Update waifu.js

--- plugins/waifu.js
+++ plugins/waifu.js
@@ -4,11 +4,11 @@ let handler = async(m, { conn }) => {
   if (!res.ok) throw await res.text()
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Istri kartun', m)
+  conn.sendFile(m.chat, json.url, '', '_Random Waifu_', m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
 handler.command = /^(waifu)$/i
 handler.limit = true
-//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+//MADE IN JAPAN
 module.exports = handler
