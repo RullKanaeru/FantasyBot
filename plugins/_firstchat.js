7ed3fbc3215db7e6465bdefbeec9c81d89dabb25
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-07 07:44:31 +0700

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -15,8 +15,8 @@ handler.before = async function (m) {
     await this.sendButton(m.chat, `
 Hai ${ucapan()} *${username.replace(/@.+/, '')}* 👋
 
-${banned ? `kamu *terbanned* kak 😕\nHubungi: wa.me/${owner[0]}` : 'Ada yang bisa saya bantu?'}
-`.trim(), wm, null, [['Menu', '/menu']], m)
+${banned ? `kamu *terbanned* kak 🐤\nHubungi: wa.me/${owner[0]}` : 'Ada yang bisa saya bantu?'}
+`.trim(), wm, null, [['Memo Bot✨', '/menu']], m)
     user.pc = new Date * 1
 }
 
