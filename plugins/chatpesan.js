87ee639c027a6eafe524eea33c70a1afa41a2155
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-07 07:46:46 +0700

    Update chatpesan.js

--- plugins/chatpesan.js
+++ plugins/chatpesan.js
@@ -2,15 +2,15 @@
 let handler = async(m, { conn, text, usedPrefix }) => {
 let [number, pesan] = text.split `|`
 
-    if (!number) return conn.reply(m.chat, 'Silahkan masukan nomor yang akan dikirim', m)
-    if (!pesan) return conn.reply(m.chat, 'Silahkan masukan pesannya', m)
+    if (!number) return conn.reply(m.chat, '🔖Silahkan masukan nomor yang akan dikirim', m)
+    if (!pesan) return conn.reply(m.chat, '📌Silahkan masukan pesannya', m)
     if (text > 500) return conn.reply(m.chat, 'Teks Kepanjangan!', m)
     
     let user = global.db.data.users[m.sender]
 
     let korban = `${number}`
     var nomor = m.sender
-    let spam1 = `*「 SUKSES 」*\n\nDari : wa.me/${korban}\nPesan : ${pesan}\n\n*${global.wm}*`
+    let spam1 = `*「 🧋 SUKSES 」*\n\nDari : wa.me/${korban}\nPesan : ${pesan}\n\n*${global.wm}*`
 
     conn.reply(korban + '@s.whatsapp.net', spam1, m)
 
