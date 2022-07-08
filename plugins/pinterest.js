34e4ac55b618c8e0c699dfe9f28e2028f70e3815
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-08 10:45:12 +0700

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -25,15 +25,15 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	  if (!text) throw `Contoh: ${usedPrefix + command} cewe cantik`
+	  if (!text) throw `Contoh: ${usedPrefix + command} Patrick Star`
 	  scrap.pinterest(text)
       .then(a => a[Math.floor(Math.random() * a.length)])
-       .then(b => conn.sendFile(m.chat,b,b,"*PINTEREST*\n\npencarian dari: " + text, m)
+       .then(b => conn.sendFile(m.chat,b,b,"*📌PINTEREST*\n\nPencarian dari: " + text, m)
     )}
     
 handler.help = ['pinterest <text>']
 handler.tags = ['internet']
-handler.command = /^(pinterest)$/i
+handler.command = /^(pinterest|pin)$/i
 handler.limit = true
 
 module.exports = handler
