6c7bb68805b4e11b48aa927db42255f2e84b3b45
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-08 13:41:09 +0700

    Update unbanuser.js

--- plugins/unbanuser.js
+++ plugins/unbanuser.js
@@ -22,11 +22,11 @@ let handler = async (m, { conn, text, usedPrefix }) => {
     return number.replace(/\s/g,'').replace(/([@+-])/g,'')
   }
 
-  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}unban number|days\n*Example:* ${usedPrefix}unban 6289654360447|99\n\n• ${usedPrefix}unban @tag|days\n*Example:* ${usedPrefix}unban @6289654360447|99`, m)
+  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}unban number|days\n*Example:* ${usedPrefix}unban 62××××××××|99\n\n• ${usedPrefix}unban @tag|days\n*Example:* ${usedPrefix}unban @6289654360447|99`, m)
   text = no(text) + "@s.whatsapp.net"
   global.db.data.users[text].banned = false
   global.db.data.users[text].bannedDate = 0
-  conn.reply(m.chat,`*Berhasil bebas dari sengsara @${text.split('@')[0]}.*`,m,{ contextInfo: { mentionedJid: [text] } })
+  conn.reply(m.chat,`*Berhasil @${text.split('@')[0]}.*`,m,{ contextInfo: { mentionedJid: [text] } })
 
 }
 handler.help = ['unban']
