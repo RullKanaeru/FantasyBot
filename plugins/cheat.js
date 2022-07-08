6aaac0df3c9c51b13a57cc32a6b7504884660bb1
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-08 13:31:06 +0700

    Update cheat.js

--- plugins/cheat.js
+++ plugins/cheat.js
@@ -2,7 +2,7 @@ let { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
-        conn.reply(m.chat, `*🌉Succes !*`, m)
+        conn.reply(m.chat, `*Sukses Cheat Di Bot, sihlakan cek inventory anda*`, m)
         global.db.data.users[m.sender].money = Infinity
         global.db.data.users[m.sender].limit = Infinity
         global.db.data.users[m.sender].level = Infinity
