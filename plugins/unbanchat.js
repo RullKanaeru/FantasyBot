b1f5322e7813f24a298787c280ee621cfe58beac
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-08 13:39:53 +0700

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   global.db.data.chats[m.chat].isBanned = false
-  m.reply('Done!')
+  m.reply('Done, bot aktif kembali di grub ini!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
