9d7df6794d5d73ffcfe4d32131c93725762df5f1
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-09 15:34:07 +0700

    Update jackpot.js

--- plugins/jackpot.js
+++ plugins/jackpot.js
@@ -30,13 +30,13 @@ let handler = async (m, { conn, usedPrefix }) => {
         let spins9 = (spin9 == 1 ? '🍊' : spin9 == 2 ? '🍇' : spin9 == 3 ? '🍉' : spin9 == 4 ? '🍌' : spin9 == 5 ? '🍍' : '' )
         let WinOrLose
         if (spin1 == 1 && spin2 == 1 && spin3 == 1 && spin4 == 1 && spin5 == 1 && spin6 == 1 && spin7 == 1 && spin8 == 1 && spin9 == 1 || spin1 == 2 && spin2 == 2 && spin3 == 2 && spin4 == 2 && spin5 == 2 && spin6 == 2 && spin7 == 2 && spin8 == 2 && spin9 == 2 || spin1 == 3 && spin2 == 3 && spin3 == 3 && spin4 == 3 && spin5 == 3 && spin6 == 3 && spin7 == 3 && spin8 == 3 && spin9 == 3 || spin1 == 4 && spin2 == 4 && spin3 == 4 && spin4 == 4 && spin5 == 4 && spin6 == 4 && spin7 == 4 && spin8 == 4 && spin9 == 4 || spin1 == 5 && spin2 == 5 && spin3 == 5 && spin4 == 5 && spin5 == 5 && spin6 == 5 && spin7 == 5 && spin8 == 5 && spin9 == 5) {
-            WinOrLose = 'BIG JACKPOT'
+            WinOrLose = '🥇BIG JACKPOT'
         } else if (spin4 == 1 && spin5 == 1 && spin6 == 1 || spin4 == 2 && spin5 == 2 && spin6 == 2 || spin4 == 3 && spin5 == 3 && spin6 == 3 || spin4 == 4 && spin5 == 4 && spin6 == 4 || spin4 == 5 && spin5 == 5 && spin6 == 5) {
-           WinOrLose = 'JACKPOT'     
+           WinOrLose = '🏅JACKPOT'     
         } else if (spin1 == 1 && spin2 == 1 && spin3 == 1 || spin1 == 2 && spin2 == 2 && spin3 == 2 || spin1 == 3 && spin2 == 4 && spin3 == 3 || spin1 == 4 && spin2 == 4 && spin3 == 4 || spin1 == 5 && spin2 == 5 && spin3 == 5 || spin1 == 6 && spin2 == 6 && spin3 == 6 || spin1 == 7 && spin2 == 7 && spin3 == 7 || spin1 == 8 && spin2 == 8 && spin3 == 8 || spin1 == 9 && spin2 == 9 && spin3 == 9) { 
-            WinOrLose = 'YOU WIN'
+            WinOrLose = '✅YOU WIN'
         } else {
-             WinOrLose = 'YOU LOSE'
+             WinOrLose = '❌YOU LOSE'
         } 
         conn.reply(m.chat, `
        *VIRTUAL SLOTS*
@@ -49,7 +49,7 @@ ${spins7}|${spins8}|${spins9}
 `.trim(), m)
     } catch (e) {
         console.log(e)
-        conn.reply(m.chat, 'Error', m)
+        conn.reply(m.chat, '❗Error', m)
         if (DevMode) {
             for (let jid of global.owner.map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').filter(v => v != conn.user.jid)) {
                 conn.sendMessage(jid, 'Menu.js error\nNo: *' + m.sender.split`@`[0] + '*\nCommand: *' + m.text + '*\n\n*' + e + '*', MessageType.text)
