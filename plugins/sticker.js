fa6f243c57517f9936321ef0260b1e5382c8d7be
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-09 15:20:46 +0700

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (/webp|image|video/g.test(mime)) {
       if (/video/g.test(mime)) if ((q.msg || q).seconds > 11) return m.reply('Maksimal 10 detik!')
       let img = await q.download()
-      if (!img) throw `⚠️balas gambar/video/stiker dengan perintah ${usedPrefix + command}`
+      if (!img) throw `balas gambar/video/stiker dengan perintah ${usedPrefix + command}`
       let out
       try {
         if (/webp/g.test(mime)) out = await webp2png(img)
@@ -24,19 +24,21 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }
     } else if (args[0]) {
       if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('❌URL tidak valid!')
+      else return m.reply('URL tidak valid!')
     }
   } catch (e) {
     console.error(e)
     if (!stiker) stiker = e
   } finally {
     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-    else throw 'Gambar Tidak Ditemukan'
+    else throw 'Conversion failed'
   }
 }
 handler.help = ['stiker (caption|reply media)', 'stiker <url>', 'stikergif (caption|reply media)', 'stikergif <url>']
 handler.tags = ['sticker']
-handler.command = /^sticker|stiker|s$/i
+handler.command = /^s(tic?ker)?(gif)?(wm)?$/i
+
+handler.limit = true
 
 module.exports = handler
 
