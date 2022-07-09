017d90b660527e5931f6b3f07d9827ccbff1f7aa
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-09 15:34:28 +0700

    Delete Ok.js

--- plugins/Ok.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "FantasyBot", author: "FantasyBot " })
-}
-
-handler.customPrefix = /^(ok|sip)$/i
-handler.command = new RegExp
-
-module.exports = handler
