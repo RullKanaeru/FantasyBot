7593a9b17c4997e13200ab3b1c6726bb358322cc
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-10 06:57:36 +0700

    Delete _halo.js

--- plugins/_halo.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://a.uguu.se/BeHAyoKM.mp4'', m, { packname: "FantasyBot", author: "FantasyBot " })
-}
-
-handler.customPrefix = /^(halo|hi|aloo)$/i
-handler.command = new RegExp
-
-module.exports = handler
