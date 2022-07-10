e369837b815a152469be0579a0b59c365157d5d0
Rull <108638335+RullKanaeru@users.noreply.github.com>
2022-07-11 06:36:35 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,10 +1,10 @@
 let handler = async(m, { conn, text, participants }) => {
   let teks = ` ╭─❑ 〔 ıll 𝘛𝖺𝗀 𝘈𝗅𝗅 llı 〕❑─
 ═══════❍ 
-*🌟 𝙋𝙀𝙎𝘼𝙉 𝘿𝘼𝙍𝙄 𝘼𝘿𝙈𝙄𝙉 :* 
+*𝙋𝙀𝙎𝘼𝙉 𝘿𝘼𝙍𝙄 𝘼𝘿𝙈𝙄𝙉 :* 
 *❑─(  ${text ? text : 'Tidak ada pesan'} )─❑*\n_______\n`
 		      	for (let mem of participants) {
-		            teks += `║┣❏🧋 @${mem.id.split('@')[0]}\n`
+		            teks += `║┣❏ @${mem.id.split('@')[0]}\n`
 				}
                 teks += `\n╰────────❑`
                 conn.sendMessage(m.chat, {text: teks, mentions: participants.map(a => a.id)}, { quoted: m })
